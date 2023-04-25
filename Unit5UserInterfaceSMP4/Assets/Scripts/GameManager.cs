c35a229446551ea5bd005186bf25c3bb5663d2d4
SMcGrath714277 <714277@student.dusd.net>
2023-04-25 11:37:16 -0700

    Update
    Completed 5.4 and Lives Bonus Track

--- Unit5UserInterfaceSMP4/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceSMP4/Assets/Scripts/GameManager.cs
@@ -9,21 +9,21 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public float spawnRate = 1.0f;
+    private int score;
+    private int lives;
+    public bool isGameActive;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
-    private int score;
-    public bool isGameActive;
+    public GameObject titleScreen;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        score = 0;
-
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+       
     }
 
     // Update is called once per frame
@@ -49,6 +49,16 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if(lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
@@ -59,5 +69,18 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
